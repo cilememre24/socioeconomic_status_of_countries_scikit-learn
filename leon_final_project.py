92feafcaedd19b05cd962ac7a75d76e150f57d89
dilanbozkurt <64786191+dilanbozkurt@users.noreply.github.com>
2021-01-22 03:46:00 +0300

    Update leon_final_project.py

--- leon_final_project.py
+++ leon_final_project.py
@@ -17,3 +17,9 @@ print("---Checking the duplicated and null values---")
 
 duplicate_check=sum(data.duplicated(subset = 'country')) == 0
 print("True if there is no duplicated value:",duplicate_check)
+
+print("--------------------------")
+
+is_null=data.isnull().sum()
+print("0 if there is no null value:")
+print(is_null)
