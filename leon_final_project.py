93b18b39452651b9e356c52da1dd2cd32a57baf3
Çilem Emre <64331364+cilememre24@users.noreply.github.com>
2021-01-22 03:45:18 +0300

    Update leon_final_project.py

--- leon_final_project.py
+++ leon_final_project.py
@@ -9,3 +9,11 @@ warnings.filterwarnings('ignore')
 #--------------GETTING THE DATA---------------
 
 data = pd.read_csv("Country-data.csv")
+
+
+#----------------PREPROCESSING---------------
+
+print("---Checking the duplicated and null values---")
+
+duplicate_check=sum(data.duplicated(subset = 'country')) == 0
+print("True if there is no duplicated value:",duplicate_check)
