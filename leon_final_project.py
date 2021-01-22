eac3c088e41fa90e41b807dc7d94f2b67faddc30
dilanbozkurt <64786191+dilanbozkurt@users.noreply.github.com>
2021-01-22 03:47:04 +0300

    Update leon_final_project.py

--- leon_final_project.py
+++ leon_final_project.py
@@ -23,3 +23,36 @@ print("--------------------------")
 is_null=data.isnull().sum()
 print("0 if there is no null value:")
 print(is_null)
+
+#According to factors, plotting the 10 countries that has the worst conditions
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+fig, axs = plt.subplots(3,3,figsize = (15,15))
+
+columns_False=['child_mort','total_fer','inflation']
+columns_True=['life_expec','health','gdpp','income','exports','imports']
+
+axsFalse=[axs[0,0],axs[0,1],axs[0,2]]
+axsTrue=[axs[1,0],axs[1,1],axs[1,2],axs[2,0],axs[2,1],axs[2,2]]
+
+for column in range(3):
+    for ax in axs:
+        top10 = data[['country',columns_False[column]]].sort_values(columns_False[column], ascending = False).head(10)
+        plt1 = sns.barplot(x='country', y=columns_False[column], data= top10, ax = axsFalse[column],palette="ch:s=-.2")
+        plt1.set(xlabel = '', ylabel= columns_False[column])
+
+for column in range(6):
+    for ax in axs:
+        top10 = data[['country',columns_True[column]]].sort_values(columns_True[column], ascending = True).head(10)
+        plt1 = sns.barplot(x='country', y=columns_True[column], data= top10, ax = axsTrue[column],palette="ch:s=-.2")
+        plt1.set(xlabel = '', ylabel= columns_True[column])
+            
+for ax in fig.axes:
+    plt.sca(ax)
+    plt.xticks(rotation = 90)
+    
+plt.tight_layout()
+plt.savefig("worst_condition.pdf")
+plt.close()
