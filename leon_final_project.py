c957d23bd05b0720fc14298c280869f7542a949b
dilanbozkurt <64786191+dilanbozkurt@users.noreply.github.com>
2021-01-22 03:50:13 +0300

    Update leon_final_project.py

--- leon_final_project.py
+++ leon_final_project.py
@@ -78,3 +78,39 @@ new_df = data[data.columns[data.dtypes != 'object']]
 
 scaler = StandardScaler()
 data_scaled = pd.DataFrame(scaler.fit_transform(new_df))
+
+#-----------------CLUSTERING TENDENCY---------------------
+
+from sklearn.neighbors import NearestNeighbors
+from random import sample
+from numpy.random import uniform
+from math import isnan
+
+
+def hopkins(X):
+    d = X.shape[1]
+    n = len(X) 
+    m = int(0.1 * n) 
+    nbrs = NearestNeighbors(n_neighbors=1).fit(X.values)
+ 
+    rand_X = sample(range(0, n, 1), m)
+ 
+    ujd = []
+    wjd = []
+    for j in range(0, m):
+        u_dist, _ = nbrs.kneighbors(uniform(np.amin(X,axis=0),np.amax(X,axis=0),d).reshape(1, -1), 2, return_distance=True)
+        ujd.append(u_dist[0][1])
+        w_dist, _ = nbrs.kneighbors(X.iloc[rand_X[j]].values.reshape(1, -1), 2, return_distance=True)
+        wjd.append(w_dist[0][1])
+ 
+    H = sum(ujd) / (sum(ujd) + sum(wjd))
+    if isnan(H):
+        print(ujd, wjd)
+        H = 0
+ 
+    return H
+
+
+print("------------------------")
+print("Hopkins score:",hopkins(new_df))
+print("------------------------")
