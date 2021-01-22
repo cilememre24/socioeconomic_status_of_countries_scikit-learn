6f4528e857d7a4bcfa4291daecb159b827218231
Çilem Emre <64331364+cilememre24@users.noreply.github.com>
2021-01-22 03:44:17 +0300

    Add files via upload

---
+++ leon_final_project.py
@@ -0,0 +1,11 @@
+#IMPORTS
+
+import numpy as np
+import pandas as pd
+
+import warnings
+warnings.filterwarnings('ignore')
+
+#--------------GETTING THE DATA---------------
+
+data = pd.read_csv("Country-data.csv")
